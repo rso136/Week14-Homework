01e6457848c7294c561ba4d57f9f23b57bde2f00
Richard Oh <rso136@gmail.com>
2016-08-07 19:57:39 -0400

    Functioning app

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Red Robin Burger');
+INSERT INTO burgers (burger_name) VALUES ('In-N-Out Burger');
+INSERT INTO burgers (burger_name) VALUES ('Shake Shack Burger');
