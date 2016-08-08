e080521ff664c9ac86e15a468fcd2f5ef1f0d501
Richard Oh <rso136@gmail.com>
2016-08-07 20:55:30 -0400

    added jawsdb code

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,23 @@
 var mysql = require('mysql');
-var connection = mysql.createConnection({
-	port: 3306,
-	host: 'localhost',
-	user: 'root',
-	password: 'orangegit12',
-	database: 'burgers_db'
-});
+
+var source = {
+
+    localhost: {
+        port: 3306,
+        host: 'localhost',
+        user: 'root',
+        password: "orangegit12",
+        database: "burgers_db"
+    },
+
+    jawsDB: {
+        port: 3306,
+        host: 'gx97kbnhgjzh3efb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user: 'e45xumklps1v6wwp',
+        password: "q6aah0uz6okkefd0",
+        database: "vqxkpdc82ckq29lm" 
+    }
+}
 
 connection.connect(function (err) {
 	if (err) {
