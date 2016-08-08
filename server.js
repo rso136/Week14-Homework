92aac1c9ff1e228b3f96d8d44dbae0c979b702a0
Richard Oh <rso136@gmail.com>
2016-08-07 21:02:53 -0400

    fixed port for heroku usage

--- server.js
+++ server.js
@@ -21,5 +21,5 @@ app.set('view engine', 'handlebars');
 var routes = require('./controllers/burgers_controller.js');
 app.use('/', routes);
 
-var port = 3000;
-app.listen(port);
+var PORT = process.env.PORT || 3000;
+app.listen(PORT);
